dc448286bdbc92a742ae6a70dbcd3baa07617381
morojuoluwa <abimbolapatrickmj@gmail.com>
2022-12-03 08:12:56 +0100

    first commit

---
+++ nav.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+const leenk = document.querySelector(".links");
+
+btn.addEventListener("click", function () {
+  leenk.classList.toggle("show-links");
+});
